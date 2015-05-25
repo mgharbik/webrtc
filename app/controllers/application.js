3ae5411b096b425d39ce0dd6a9825a9348a34d49
Mohammed Gharbi <simo163@hotmail.co.uk>
2015-05-25 13:03:44 +0200

    showing webcam in screen using getUserMedia

---
+++ app/controllers/application.js
@@ -0,0 +1,48 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	constraintsList: ['HD, VGA'],
+	selectedConstraints: 'HD',
+	constraints: null,
+
+	getConstraints: function(){
+		if (this.get('selectedConstraints') == 'HD') {
+			return {
+				video: {
+		    		mandatory: {
+						minWidth: 1280,
+		      		  	minHeight: 720
+		    		}
+		  	  	}
+			};
+		} else if (this.get('selectedConstraints') == 'VGA')  {
+			return {
+				video: {
+					mandatory: {
+		      	  		maxWidth: 640,
+		      	  		maxHeight: 360
+		    		}
+		  	  }
+			};
+		}
+	},
+
+	didConstraintsChanged: function(){
+		this.setUserMedia();
+	}.observes('selectedConstraints'),
+
+	setUserMedia: function(){
+		navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+		navigator.getUserMedia(this.getConstraints(), this.successCallback, this.errorCallback);
+	},
+
+	successCallback: function(stream){
+		var video = document.querySelector('video');
+		video.src = window.URL.createObjectURL(stream);
+	},
+
+	errorCallback: function(error){
+		console.log('navigator.getUserMedia error: ', error);
+	}
+
+});
---
+++ app/views/application.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+
+	didInsertElement: function(){
+		this.get('controller').setUserMedia();
+	}
+
+});
